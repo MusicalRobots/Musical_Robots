bd7102c5f6d3e7ed4f34795a7af271ccf67646c3
KatieJ16 <katie@acornonsite.com>
2021-11-09 08:57:02 -0800

    clean up some

--- data_generation.py
+++ data_generation.py
@@ -12,22 +12,17 @@ import ast
 #load data, transform to spectrograms and save to training data
 
 file_paths = pd.read_csv('data/all_data_paths.txt', header = None, names = ['file_path'])
-
-# train_paths, test_paths = train_test_split(file_paths, test_size=0.33)
-
+# train_paths, test_paths = train_test_split(file_paths, test_size=0.33) #might want to randomize instead, right now, picking first n
 music_data = pd.read_csv('data/fma_metadata/tracks.csv', skiprows = [0,1,2], 
                          usecols = [0, 6, 8, 11, 26, 39, 41, 44, 47, 52], 
                          names = ['track_id', 'album_id', 'album_listens', 'album_title', 'artist_name',
                                  'track_favorites', 'track_genres', 'track_interest', 'track_listens', 'track_title'])
-
 music_data['track_genres'] = music_data['track_genres'].apply(ast.literal_eval)
-
-# music_data.head()
 
 
 #get spectrograms and save training data
-items_to_keep = 100000
-training_points = 50
+items_to_keep = 100000 #length to keep, might want to make this longer, but also takes much longer
+training_points = 50 #number training points, will need to make this bigger later
 labels = np.zeros(training_points)
 train_data = np.zeros((training_points, items_to_keep))
 for index in range(training_points): #just looks at top 100 files
@@ -43,7 +38,7 @@ for index in range(training_points): #just looks at top 100 files
     song_id = file_paths['file_path'][index].rsplit('/')[1].rsplit('.')[0].lstrip('0')
     associated_genres_numeric = music_data[music_data['track_id'] == int(song_id)]['track_genres'].item()
     
-    labels[index] = associated_genres_numeric[0]
+    labels[index] = associated_genres_numeric[0] #right now only keeping first label, will need to make it keep all labels later
     
 #save training data and labels
 np.save('data/train_data.npy', train_data)
