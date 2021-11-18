3a5897539afdd171d9821c54f2332046b29581ed
Olga Dorabiala <Olga400916@gmail.com>
2021-11-18 10:49:18 -0800

    create mfcc dataset, change existing dataset

--- SpectrogramDataset.py
+++ SpectrogramDataset.py
@@ -8,7 +8,6 @@ import warnings
 import librosa
 from typing import Tuple
 from sklearn.model_selection import train_test_split
-import ast
 
 warnings.filterwarnings("ignore")
 
@@ -39,19 +38,86 @@ class SpectrogramDataset(Dataset):
                 print('Failed to load ', filename)
                 continue
 
-            genre_label = np.zeros(len(np.unique(genre_df['genre_id'])))
+            genre_label = np.zeros(genre_df['genre_id'].nunique())
 
-            mel = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=512)
+            mel = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=1024)
             mel = torch.from_numpy(mel)
-            m = torch.nn.AdaptiveAvgPool1d(2584)
+            m = torch.nn.AdaptiveAvgPool1d(1292)
             mel = m(mel)
+            mel = (mel - torch.min(mel))/(torch.max(mel) - torch.min(mel))
 
             song_id = row[1].rsplit('/')[1].rsplit('.')[0].lstrip('0')
-            genre_numeric = music_df[music_df['track_id'] == int(song_id)]['track_genres'].item()
-            index = genre_df[genre_df['genre_id'] == genre_numeric[0]].index.item()
-            # We can change this to be the whole list instead of just the first element.
-            # ind = genre_df[genre_df['genre_id'].isin(genre_numeric)].index
+
+            try:
+                genre_text = music_df[music_df['track_id'] == int(song_id)]['track_genre_top'].item()
+            except ValueError:
+                continue
+
+            index = genre_df[genre_df['title'] == genre_text].index.item()
             genre_label[index] = 1
+            
+            genre_label = torch.from_numpy(genre_label)
+
+            self.samples.append((mel, genre_label))
+
+    def __len__(self) -> int:
+        """Return length of dataset."""
+        return len(self.samples)
+
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Return a single spectrogram and its label from the dataset.
+
+        Args:
+            idx: (int) Index of dataset item to return.
+        """
+        return self.samples[idx]
+
+
+class MfccDataset(Dataset):
+    """Create custom dataset of spectrograms and genre labels."""
+
+    def __init__(self, path_df: pd.DataFrame, music_df: pd.DataFrame, genre_df: pd.DataFrame) -> None:
+        """
+        Create list of data tuples.
+
+        Args:
+            path_df: (pd.Dataframe) DataFrame containing file paths.
+            music_df: (pd.DataFrame) Dataframe containing music information.
+            genre_df: (pd.DataFrame) Dataframe containing genre information.
+
+        Attributes:
+            self.samples: (List[Tuple[librosa.feature.melspectrogram, str]]) List of data tuples.
+        """
+        self.samples = []
+
+        for row in path_df.itertuples():
+            filename = 'data/fma_small/' + row[1]
+
+            try:
+                y, _ = librosa.load(filename, sr=None, mono=True)
+            except (RuntimeError, audioread.NoBackendError):
+                print('Failed to load ', filename)
+                continue
+
+            genre_label = np.zeros(genre_df['genre_id'].nunique())
+
+            mel = librosa.feature.mfcc(y=y)
+            m = torch.nn.AdaptiveAvgPool1d(2500)
+            mel = m(torch.from_numpy(mel))
+            mel = (mel - torch.min(mel)) / (torch.max(mel) - torch.min(mel))
+
+            song_id = row[1].rsplit('/')[1].rsplit('.')[0].lstrip('0')
+            try:
+                genre_text = music_df[music_df['track_id'] == int(song_id)]['track_genre_top'].item()
+            except ValueError:
+                continue
+
+            index = genre_df[genre_df['title'] == genre_text].index.item()
+
+            genre_label[index] = 1
+
+            genre_label = torch.from_numpy(genre_label)
 
             self.samples.append((mel, genre_label))
 
@@ -88,16 +154,21 @@ def create_dataframes(file_paths_path: str, tracks_csv_path: str, genre_csv_path
     file_path_df = pd.read_csv(file_paths_path, header=None, names=['file_path'])
 
     track_df = pd.read_csv(tracks_csv_path, skiprows=[0, 1, 2],
-                           usecols=[0, 6, 8, 11, 26, 39, 41, 44, 47, 52],
+                           usecols=[0, 6, 8, 11, 26, 39, 40, 41, 44, 47, 52],
                            names=['track_id', 'album_id', 'album_listens', 'album_title', 'artist_name',
-                                  'track_favorites', 'track_genres', 'track_interest', 'track_listens',
-                                  'track_title'])
+                                  'track_favorites', 'track_genre_top', 'track_genres', 'track_interest',
+                                  'track_listens', 'track_title'])
 
-    track_df['track_genres'] = track_df['track_genres'].apply(ast.literal_eval)
+    track_df = track_df[track_df['track_genre_top'].notna()]
 
     genre_df = pd.read_csv(genre_csv_path, usecols=[0, 3])
+    relevant_indices = []
+    for genre in track_df['track_genre_top'].unique():
+        relevant_indices.append(genre_df[genre_df['title'] == genre].index.item())
+    relevant_genre_df = genre_df.iloc[relevant_indices]
+    relevant_genre_df.reset_index(inplace=True)
 
-    return file_path_df, track_df, genre_df
+    return file_path_df, track_df, relevant_genre_df
 
 
 def create_dataset(file_path_df: pd.DataFrame, track_df: pd.DataFrame, genre_df: pd.DataFrame,
@@ -131,6 +202,43 @@ def create_dataset(file_path_df: pd.DataFrame, track_df: pd.DataFrame, genre_df:
     print('Validation dataset created.')
 
     test_data = SpectrogramDataset(test_paths, track_df, genre_df)
+
+    print('Test dataset created.')
+
+    return train_data, validation_data, test_data
+
+
+def create_mfcc_dataset(file_path_df: pd.DataFrame, track_df: pd.DataFrame, genre_df: pd.DataFrame,
+                        test_percentage: float = .10,
+                        validation_percentage: float = .10) -> Tuple[Dataset, Dataset, Dataset]:
+    """
+    Create the custom dataset given the locations of the data.
+
+    Args:
+        file_path_df: (pd.DataFrame) Dataframe storing the data paths for each sound file.
+        track_df: (pd.DataFrame) Dataframe storing general track data.
+        genre_df: (pd.Dataframe) Dataframe storing genre information.
+        test_percentage: (float) Percentage of paths to designate as part of the test dataset.
+        validation_percentage: (float) Percentage of paths to designate as part of the validation dataset.
+
+    Returns:
+        train_data: (Dataset) Dataset containing training music data as spectrograms and genre labels.
+        validation_data: (Dataset) Dataset containing validation music data as spectrograms and genre labels.
+        test_data: (Dataset) Dataset containing testing music data as spectrograms and genre labels.
+    """
+
+    train_paths, test_paths = train_test_split(file_path_df, test_size=test_percentage)
+    train_paths, validation_paths = train_test_split(train_paths, test_size=validation_percentage)
+
+    train_data = MfccDataset(train_paths, track_df, genre_df)
+
+    print('Training dataset created.')
+
+    validation_data = MfccDataset(validation_paths, track_df, genre_df)
+
+    print('Validation dataset created.')
+
+    test_data = MfccDataset(test_paths, track_df, genre_df)
 
     print('Test dataset created.')
 
