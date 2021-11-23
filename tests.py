9bec3e11832026b39cdc1638c2e103ae9f5e2396
KatieJ16 <katie@acornonsite.com>
2021-11-23 13:30:35 -0800

    adding some tests

--- tests.py
+++ tests.py
@@ -1,11 +1,14 @@
+import unittest
 import pandas as pd
 import librosa
 import librosa.display
 
+import SpectrogramDataset
 
-def load_test():
-    # check that the data can load, (getting an error in Windows)
-    try:
+class Testknn(unittest.TestCase):
+
+    def test_load(self):
+        # check that the data can load, (getting an error in Windows)
         # read input file
         file_paths = pd.read_csv('data/all_data_paths.txt', header=None, names=['file_path'])
 
@@ -13,38 +16,36 @@ def load_test():
         index = 7999
         filename = 'data/fma_small/' + file_paths['file_path'][index]
         y, sr = librosa.load(filename, sr=None, mono=True)
-    except:
-        # fails is any error pops up
-        print("load_test failed.")
-        return
-    
-    # passed when successfully ran all code
-    print("load_test_passed.")
-    return
 
-"""
-def test_queries():
-    try: #both should fail bc k not in (1,11)
-        return_similar_genres(genre: 'pop', genre_df: pd.DataFrame, track_df: pd.DataFrame, k: int = 0)
-        
-    try: should fail bc k not in (1,11)
-        return_similar_genres(genre: 'pop', genre_df: pd.DataFrame, track_df: pd.DataFrame, k: int = 11)
-    
-    try: genre should not exist
-        return_similar_genres(genre='screeching cats', genre_df: pd.DataFrame, track_df: pd.DataFrame, k: int = 10)
-        return_most_popular_song(genre: 'screeching cats', genre_df, track_df)
-        play_random_song_from_genre(genre= 'screeching cats', genre_df, track_df, path_df)
-        
-    try: should return none bc audio not in dataset
-        play_song_from_title(title = '#1', track_df, path_df)
-        
-        should play 
-        play_song_from_title(title= 'This World', track_df, path_df)
-        should fail bc title does not exist
-        play_song_from_title(title= 'Blah', track_df, path_df)    
+    def test_make_SpectrogramDataset(self):
+        """ Test making a dataframe with a very shorted version of the data
+        """
+        file_path_df, track_df, genre_df = SpectrogramDataset.create_dataframes(file_paths_path = 'data/all_data_path_short.txt' ,
+                                                     tracks_csv_path = 'data/fma_metadata/tracks_short.csv',
+                                                     genre_csv_path = 'data/fma_metadata/genres.csv')
+
+    # SpectrogramDataset(Dataset):
+    #     """Create custom dataset of spectrograms and genre labels."""
+    #
+    #     def __init__(self, path_df: pd.DataFrame, music_df: pd.DataFrame, genre_df: pd.DataFrame) -> None:
     """
+    def test_queries():
+        try: #both should fail bc k not in (1,11)
+            return_similar_genres(genre: 'pop', genre_df: pd.DataFrame, track_df: pd.DataFrame, k: int = 0)
 
+        try: should fail bc k not in (1,11)
+            return_similar_genres(genre: 'pop', genre_df: pd.DataFrame, track_df: pd.DataFrame, k: int = 11)
 
+        try: genre should not exist
+            return_similar_genres(genre='screeching cats', genre_df: pd.DataFrame, track_df: pd.DataFrame, k: int = 10)
+            return_most_popular_song(genre: 'screeching cats', genre_df, track_df)
+            play_random_song_from_genre(genre= 'screeching cats', genre_df, track_df, path_df)
 
-#run all the tests when file is ran
-load_test()
+        try: should return none bc audio not in dataset
+            play_song_from_title(title = '#1', track_df, path_df)
+
+            should play
+            play_song_from_title(title= 'This World', track_df, path_df)
+            should fail bc title does not exist
+            play_song_from_title(title= 'Blah', track_df, path_df)
+        """
