3dc358af5cb5e114682c79908ec042e1734fc088
Olga Dorabiala <Olga400916@gmail.com>
2021-12-08 14:32:29 -0800

    reformat for flake8

--- musical_robots/__init__.py
+++ musical_robots/__init__.py
@@ -3,3 +3,7 @@ from .spectrogram_dataset import create_dataframes, AudioFeature, split_data, \
 from .svm_prediction import svm_prediction
 from .dataset_queries import return_similar_genres, return_most_popular_song
 from .interact_with_user import Interactive
+
+__all__ = [create_dataframes, AudioFeature, split_data,
+           create_audio_feature_dataset, svm_prediction, return_similar_genres,
+           return_most_popular_song, Interactive]
