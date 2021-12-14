155c7a2bb99223c11c4d85b8d2dd86dba9250a0e
raph651 <raph651@uw.edu>
2021-12-14 00:44:20 -0800

    fix issues in demo.py, now a session_state.py is added for streamlit flow control

--- musical_robots/dataset_queries.py
+++ musical_robots/dataset_queries.py
@@ -223,7 +223,7 @@ def play_random_song_from_genre(
 
             status = True
 
-        if max_lookups > 10:
+        if max_lookups > 20:
             print("Could not find song in genre.")
             return audio, song_title, artist_name, album_title
 
--- musical_robots/demo.py
+++ musical_robots/demo.py
@@ -1,18 +1,16 @@
 """Implementation of interactive class to communicate with user."""
 import streamlit as st
-import ipywidgets as widgets
 import warnings
 import ast
 import audioread
 import librosa
-import IPython.display as ipd
 import pandas as pd
 import numpy as np
 
 from dataset_queries import return_similar_genres, return_most_popular_song, \
     play_random_song_from_genre, play_song_from_filename
 from svm_prediction import svm_prediction
-from IPython.display import display, HTML, clear_output
+from session_state import _get_state
 
 from typing import Tuple, List
 
@@ -58,19 +56,27 @@ class Interactive:
         #self.music_file_content= None
         
         pass
-    @st.cache(suppress_st_warning=True)
+    #@st.cache(suppress_st_warning=True)
     def start_up(self):
         """
-        Start up the interaction. Create and display a file uploader.
+        Start up the interaction.
         
             
         """
         st.title("Hi there. I'm the musical robot. Nice to meet you!")
+        return self.upload_and_save()
+        
+    def upload_and_save(self):
+        """
+        Create and display a file uploader.
+        
+            
+        """
         st.write("If you want to know the genre of the music, please go ahead and upload your music/audio file below.")    
+        uploader=st.file_uploader(label="",key=st.session_state.upload_try_time,accept_multiple_files=False)
         
-        uploader=st.file_uploader(label="",accept_multiple_files=False)
-        
-        if uploader is not None :
+        if uploader is not None or st_state.uploaded:
+            st_state.uploaded = True
             st.session_state.uploaded_filename = uploader.name
             st.session_state_content = uploader.getvalue()
             with open(f"./{st.session_state.uploaded_filename}", "wb") as fp:
@@ -80,7 +86,9 @@ class Interactive:
                 try:
                     y_audio, sample_rate = \
                         librosa.load(st.session_state.uploaded_filename)
+                    return self.if_play_songs_first()
                 except (RuntimeError, TypeError, audioread.NoBackendError):
+                    st.session_state.upload_try_time+=1
                     st.write(
                             "Sorry, the file you uploaded is not "
                             "compatible, please check your music/audio"
@@ -89,11 +97,12 @@ class Interactive:
                     return self.upload_and_save()
         else:
             st.stop()
-    @st.cache(suppress_st_warning=True)
+    #@st.cache(suppress_st_warning=True)
     def if_play_songs_first(self):
         """
         Create and display yes-or-no buttons.
         Users choose if they want to listen to the song first.
+        
         """
        
              
@@ -101,28 +110,34 @@ class Interactive:
                 "Before analysing the genre, do you want to play your"
                 " audio file?"
             )
-        if(st.button("Yes",key=1)):
+        if st.button("Yes",key=1) or st_state.yes_play_audio_first:
+            st_state.yes_play_audio_first = True
             st.write(
                     "Gotcha! Gonna play the audio for you! "
                 )
             st.audio(st.session_state_content)
-            if(st.button("End",key=3)):
+            if st.button("Proceed",key=3) or st_state.proceed_1 :
+                st_state.proceed_1 = True
                 st.write("Start analysing...")
+                return self.call_out_genre_prediction()
             else:
                 st.stop()
-        elif(st.button("No",key=2)):
+        elif st.button("No",key=2) or st_state.no_and_proceed:
+            st_state.no_and_proceed = True
             st.write(
                     "Ok, analysing the genre "
                     "for you"
                 )
+            return self.call_out_genre_prediction()
+
         else:
             st.stop()
-    @st.cache(suppress_st_warning=True)
+    #@st.cache(suppress_st_warning=True)
     def call_out_genre_prediction(self) -> None:
         """
         Return predicted song genre, similar genres, and most popular song.
         Args:
-            output (widgets.Output):
+        
         """
         file_path_df = pd.read_csv("./data/file_path_df", index_col=0)
         track_df = pd.read_csv("./data/track_df", index_col=0)
@@ -153,20 +168,23 @@ class Interactive:
         st.session_state.genre_df =genre_df
         st.session_state.total_genre_df=total_genre_df
         
-        if (st.button("Yes",key=4)):
-            pass
-        elif (st.button("No",key=5)):
+        if st.button("Yes",key=4) or st_state.yes_similar_songs:
+            st_state.yes_similar_songs = True
+            return self.yes_similar_songs(st.session_state.file_path_df)
+        elif st.button("No",key=5)or st_state.no_and_end_interaction_1:
+            st_state.no_and_end_interaction_1 = True
             return self.no_similar_songs()
         else:
             st.stop()
-    @st.cache(suppress_st_warning=True)
+    #@st.cache(suppress_st_warning=True)
     def no_similar_songs(self):
         st.success("Hope this was helpful ! Thank you !")
         st.stop()
         pass
-    @st.cache(suppress_st_warning=True)
+    #@st.cache(suppress_st_warning=True)
     def yes_similar_songs(self,file_path_df):
         """Display the most popular songs and the most similar genres."""
+        
         most_popular_song_ids,most_popular_song,most_popular_artist, most_popular_album = return_most_popular_song(genre=st.session_state.genre, genre_df=st.session_state.total_genre_df, track_df=st.session_state.track_df)
         st.write(
                 "The most popular songs in the genre {} are {} by {}"
@@ -179,15 +197,16 @@ class Interactive:
         st.session_state.most_similar_genres = return_similar_genres(
              genre=st.session_state.genre, genre_df=st.session_state.total_genre_df, track_df=st.session_state.track_df, k=10
              )
-        st.write("The most similar genres to {} are : {} <h3>".format(
+        st.write("The most similar genres to {} are : {}".format(
                st.session_state.genre, ", ".join(st.session_state.most_similar_genres)
                  )
              )
-        st.write("Would you like to hear the most popular song "
+        st.title("Would you like to hear the most popular song "
                "{}".format(most_popular_song[0])
              )
 
-        if (st.button("Yes",key=6)):
+        if st.button("Yes",key=6) or st_state.yes_hear_most_popular_song:
+            st_state.yes_hear_most_popular_song = True
             st.write("Gotcha! Gonna playing the most popular "
                  "song for you! "
              )
@@ -196,19 +215,21 @@ class Interactive:
             file_path = file_path_df[file_path_df["file_path"].str.contains(song_id)]
 
             if len(file_path) != 0:
-                filename = '/mnt/e/fma_small/' + file_path['file_path'].item()
+                filename = 'data/fma_small/' + file_path['file_path'].item()
                 st.audio(filename)
-                if( st.button("End",key=8)):
-                    pass
+                if st.button("Proceed",key=8) or st_state.proceed_2:
+                    st_state.proceed_2 = True
+                    return self.ask_if_hear_a_random_song()
                 else:
                     st.stop()
             else:
                 st.write(
                         "Sorry, the audio could not be "
-                        "found! <h2>".format()
+                        "found!".format()
                     )
-        elif (st.button("No",key=7)):
-            pass
+        elif st.button("No",key=7) or st_state.no_ask_if_hear_random_song:
+            st_state.no_ask_if_hear_random_song = True
+            return self.ask_if_hear_a_random_song()
         else:
             st.stop()
         
@@ -218,9 +239,11 @@ class Interactive:
                ", ".join(st.session_state.most_similar_genres)
                  )
              )
-        if (st.button("Yes",key=9)):
-            pass
-        elif (st.button("No",key=10)):
+        if st.button("Yes") or st_state.yes_play_random_song_from_genre:
+            st_state.yes_play_random_song_from_genre = True
+            return self.yes_play_random_song_from_genre()
+        elif st.button("No") or st_state.no_and_end_interaction_2:
+            st_state.no_and_end_interaction_2 = True
             return self.end_interaction()
         else:
             st.stop()
@@ -231,7 +254,7 @@ class Interactive:
         
     def yes_play_random_song_from_genre(self):
         st.write("Which genre would you like to hear a "
-                  "random song from: {} ?<h2>".format(
+                  "random song from: {} ?".format(
                   ", ".join(st.session_state.most_similar_genres)
                 )
             )
@@ -247,26 +270,30 @@ class Interactive:
                  path_df=st.session_state.file_path_df
                 )
             if random_song is not None:
-                st.write("Playing song {} by {} from {} .<h2>".format(
+                st.write("Playing song {} by {} from {} .".format(
                              random_song[1], random_song[2], random_song[3]
                         )
                     )
                 st.write(random_song[0])
+                
             else:
                 st.write(
                        "Sorry, could not find a song from this "
                        "genre."
                     )
-            if (st.button("Play another random song?",key=11)):
-                return self.yes_play_random_song_from_genre()
-            if (st.button("No",key=12)):
-                return self.no_dont_play_random_song_from_genre()
+           # if st.button("Play another random song?",key=9) or st_state.play_another_random_song:
+          #      st_state.play_another_random_song = True
+          #      return self.yes_play_random_song_from_genre()
+            if st.button("End interaction",key=10) or st_state.end_interaction_3:
+                st_state.end_interaction_3 = True
+                return self.end_interaction()
         
 
         
         
 if not "initialized" in st.session_state:
     st.session_state.initialized = True
+    st.session_state.upload_try_time=1
     st.session_state.uploaded_filename = None
     st.session_state_content = None
     st.session_state.genre = None
@@ -276,49 +303,8 @@ if not "initialized" in st.session_state:
     st.session_state.file_path_df = None
     
 robot=Interactive()
-#col1, col2 = st.sidebar.columns([1, 1])
 
-robot.start_up()
+if st.session_state.initialized:
+    st_state=_get_state()
+    robot.start_up()
 
-robot.if_play_songs_first()
-robot.call_out_genre_prediction()
-robot.yes_similar_songs(st.session_state.file_path_df)
-robot.ask_if_hear_a_random_song()
-robot.yes_play_random_song_from_genre()
-robot.end_interaction()
-
-
-#    def style_button_row(self, clicked_button_ix, n_buttons):
-#        def get_button_indices(button_ix):
-#            return {
-#                'nth_child': button_ix,
-#                'nth_last_child': n_buttons - button_ix + 1
-#            }
-#        clicked_style = """
-#                    div[data-testid*="stHorizontalBlock"] > div:nth-child(%(nth_child)s): 
-#                    nth-last-child(%(nth_last_child)s) button {
-#                    border-color: "
-#                    rgb(255, 75, 75);
-#                    color: rgb(255, 75, 75);
-#                    box-shadow: rgba(255, 75, 75, 0.5) 0px 0px 0px 0.2rem;
-#                    outline: currentcolor none medium;
-#                }
-#                """
-#        unclicked_style = """
-#                    div[data-testid*="stHorizontalBlock"] > div:nth-child(%(nth_child)s):nth-last-child(%(nth_last_child)s) button {
-#                    pointer-events: none;
-#                    cursor: not-allowed;
-#                    opacity: 0.65;
-#                    filter: alpha(opacity=65);
-#                    -webkit-box-shadow: none;
-#                    box-shadow: none;
-#                }
-#                """
-#        style = ""
-#        for ix in range(n_buttons):
-#            ix += 1
-#            if ix == clicked_button_ix:
-#                style += clicked_style % get_button_indices(ix)
-#            else:
-#                style += unclicked_style % get_button_indices(ix)
-#        st.markdown(f"<style>{style}</style>", unsafe_allow_html=True)
